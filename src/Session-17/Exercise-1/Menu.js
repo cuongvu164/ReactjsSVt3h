7f4e362d8a045a5edf67173f6ee88c5a1bbc92a2
cuongcuoi <cuongmk288@gmail.com>
2020-11-29 23:09:07 +0700

    Add file homework-17

---
+++ src/Session-17/Exercise-1/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './main.js'
+import './style.css'
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <div id="box">
+
+      </div>
+    )
+  }
+}
+
+export default Menu
